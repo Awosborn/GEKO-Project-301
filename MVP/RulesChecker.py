c16e764ea70bb3f9bf5db559de19676391df67ae
Awosborn <awosborn@asu.edu>
2026-04-07 23:53:46 -0700

    Create RulesChecker.py with initial comments
    Add initial comments for RulesChecker.py to outline its purpose.

---
+++ MVP/RulesChecker.py
@@ -0,0 +1,2 @@
+#this file is ment to ensure that bids made by our machine learning model not built yet actually follow the appropriate functionality.
+# in this file build a checker that ensures that a bid follows the strategy decleration.
