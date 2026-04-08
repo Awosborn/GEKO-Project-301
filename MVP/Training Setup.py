b7b84b3c907a05390c79e6076a436489e28e4034
Awosborn <awosborn@asu.edu>
2026-04-07 23:07:43 -0700

    Enhance comments in Training Setup.py
    Added comments to clarify the purpose and steps for data processing.

---
+++ MVP/Training Setup.py
@@ -0,0 +1,4 @@
+#Take the data from data.py and turn it into numerics that can be sent to a trainer by doing the following
+#1 make all bids have their own associated token that has a vector of random weights
+#2 have each card have an assocated token that has a vector of random weights
+#turn the strategy decleration question each into a token that has a vector of random weights
