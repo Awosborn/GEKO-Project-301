88b3170be1898b717fe8c8f06d939fe4f806fe68
Awosborn <awosborn@asu.edu>
2026-04-07 23:36:53 -0700

    Fix comment on model output process
    Corrected the output description in the model's comments.

---
+++ MVP/BiddingModel.py
@@ -0,0 +1,8 @@
+#this model takes in the cleanned data from training setup.py and then creates a bridge playing model that is effective at bidding
+#this model will have the following [token 1 the strategy decleration of its partner, the first bid.. second bid ... ect]
+#this model will process these simillar to an LLM in which it embeds the vectors with meaning has an attention layer feedfoward layer and is repeated 25 times per descion. Then based on the last result it will output the result
+#Creates Embeding vector Using tokens
+#moves to attention layer
+#creates feed foward layer
+# repeate 25 times per descion
+#
