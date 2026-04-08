4c889d620d6451ca427c49c2eb444daf41458396
Awosborn <awosborn@asu.edu>
2026-04-07 23:55:01 -0700

    Update comments in RulesChecker.py for clarity
    Expanded comments to clarify the purpose of the file.

--- MVP/RulesChecker.py
+++ MVP/RulesChecker.py
@@ -1,2 +1,3 @@
 #this file is ment to ensure that bids made by our machine learning model not built yet actually follow the appropriate functionality.
-# in this file build a checker that ensures that a bid follows the strategy decleration.
+# in this file build a checker that ensures that a bid follows the strategy decleration. this is one function
+# in this file build a checker that ensures that a card play follows the strategy decleration. this is another
