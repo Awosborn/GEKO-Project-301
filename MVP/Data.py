24bbb45835598fc3c2ce5140c73c63b89645ddc7
sgarteiz9090 <sgarteiz@asu.edu>
2026-04-07 17:21:00 -0700

    Update and rename SetupClasses.py to Data.py

---
+++ MVP/Data.py
@@ -0,0 +1,6 @@
+#Data will store all the following data for each game 
+#Define Stratagey Decleration of partner player to use as rules for bidding stage in game loop
+#Curr_Card_Hold this will be a vector 4 by 13 with the values meaning which card each user has been given 
+#Curr_Bid_Hist this will store the bids that each input made durring bidding stage
+#Curr_Points will store how many points the users got in that round of game loop
+#Hist_Points will store how many points the users have in total
