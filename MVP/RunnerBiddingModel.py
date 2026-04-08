a9f6c1d2ce731ae3e76772cd3c3f0363d54f3baf
Awosborn <awosborn@asu.edu>
2026-04-08 00:50:49 -0700

    Add initial comments to RunnerBiddingModel.py

---
+++ MVP/RunnerBiddingModel.py
@@ -0,0 +1,6 @@
+#The bot will initally learn by playing a human player
+#The bot will be made up of the following 25 attention and feed foward layers
+#after the prediction is made from the attention and feedfoward layers it will then quirey the human player
+#the bot will play and its score will be subtracted from the double dummy function in game loop
+#the bot will play as one of the players for as many times until the player ends it
+#the bot error will then be caulculated by comparing it to double dummy if it is above then loss will be proptinall 20*each level above and 10*each level and missin game is -190
